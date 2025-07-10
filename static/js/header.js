c70e4082df18ce7af53a5b11d7d913f00ddabd76
arboldemelones <jjlopezcastilla@gmail.com>
2025-07-10 15:00:06 +0200

    Dither canvas

---
+++ static/js/header.js
@@ -0,0 +1,38 @@
+import { intBufferFromCanvas } from "@thi.ng/pixel";
+import { ditherWith, ATKINSON } from "@thi.ng/pixel-dither";
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+let width = 300;
+let height = 150;
+
+const observer = new ResizeObserver((entries) => {
+  width = canvas.clientWidth;
+  height = 150;
+});
+observer.observe(canvas)
+
+// not import but draw something just to showcase
+
+const grad=ctx.createLinearGradient(0,0, 280,0);
+grad.addColorStop(0, "lightblue");
+grad.addColorStop(1, "darkblue");
+
+function render(time) {
+  
+  const range = Math.max(width, height) * 0.8;
+  const size = 64 + Math.sin(time * 0.001) * 50;
+  for (let i = 0; i < range; i += size) {
+    ctx.fillStyle = grad;
+    ctx.fillRect( i, -range, size, range * 2);
+    ctx.fillRect(-i, -range, size, range * 2);
+  }
+  
+  ctx.restore();
+
+  requestAnimationFrame(render)
+}
+requestAnimationFrame(render);
+
+const img = intBufferFromCanvas(canvas);
+ditherWith(ATKINSON, img);
